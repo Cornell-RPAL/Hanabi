e018e70199aeb6f967471869abd8238bd47ea38c
Josh Zheng <jz786@cornell.edu>
2019-06-19 14:26:15 -0400

    start SelfKnowledge

--- game/selfKnowledge.py
+++ game/selfKnowledge.py
@@ -1,1 +1,12 @@
+from game import Game, ALL_CARDS
+from unknownCard import UnknownCard
+from collections import Counter
+
 class SelfKnowledge():
+    def __init__(self, game, player):
+        self._board = game.board
+        self._partnerHand = Counter(game.getHand(1 - player))
+        self._hand = Counter(ALL_CARDS)
+        self._hand.subtract(self._partnerHand)
+
+    def 
