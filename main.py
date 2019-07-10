ba30d4d4a2984a61b5208d78228cde95eeab5595
Josh Zheng <joshzheng@dhcp-rhodes-2047.redrover.cornell.edu>
2019-07-10 13:03:10 -0400

    Finally make 1 process and 1 async run together

--- main.py
+++ main.py
@@ -10,32 +10,33 @@ class Main():
         self._oldLength = 1
         
 
-    def _display(self):
-        #while True:
-        #await asyncio.sleep(50)
+    async def _display(self):
         while True:
-            #print ("test")
-            if len(self._sensoryBuffer.text) != self._oldLength:
-                print("hi")
-                print (self._sensoryBuffer.text)
-                self._oldLength = len(self._sensoryBuffer.text)
+            await asyncio.sleep(0.5)
+            print ("test")
+            # if len(self._sensoryBuffer.text) != self._oldLength:
+            #     print("hi")
+            #     print (self._sensoryBuffer.text)
+            #     self._oldLength = len(self._sensoryBuffer.text)
     
-    def run(self):
+    async def run(self):
         #v2t = asyncio.create_task(v2tloop(self._sensoryBuffer))
-        # d = loop.create_task(self._display())
-        # await asyncio.gather(d)
+        d = asyncio.create_task(self._display())
+        
 
         executor = concurrent.futures.ProcessPoolExecutor()
         executor.submit(v2tloop, self._sensoryBuffer)
         #executor.submit(self._display)
+
+        await asyncio.gather(d)
         # with concurrent.futures.ProcessPoolExecutor() as pool:
         #     await loop.run_in_executor(pool, v2tloop(self._sensoryBuffer))
         # with concurrent.futures.ProcessPoolExecutor() as pool:
         #     await loop.run_in_executor(pool, self._display)
 
 m = Main()
-m.run()
+#m.run()
 #loop = asyncio.get_event_loop()
 #loop.run_until_complete(m.run())
 
-#asyncio.run(m.run())
+asyncio.run(m.run())
