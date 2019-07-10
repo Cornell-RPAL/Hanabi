0fde7c34eb70b4fad4e8d0fbcf230e3363d388f9
Josh Zheng <joshzheng@dhcp-rhodes-2047.redrover.cornell.edu>
2019-07-10 14:08:58 -0400

    Use pipe to send data between processes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import concurrent.futures
+from multiprocessing import Process, Queue, Pipe
 
 from sensoryBuffer import SensoryBuffer
 from voice.voice_stream_to_text import main as v2tloop
@@ -9,26 +10,38 @@ class Main():
         self._sensoryBuffer = SensoryBuffer()
         self._oldLength = 1
         
-
     async def _display(self):
         while True:
             await asyncio.sleep(0.5)
-            print ("test")
-            # if len(self._sensoryBuffer.text) != self._oldLength:
-            #     print("hi")
-            #     print (self._sensoryBuffer.text)
-            #     self._oldLength = len(self._sensoryBuffer.text)
+            print (self._sensoryBuffer.text)
+            if len(self._sensoryBuffer.text) != self._oldLength:
+                print("hi")
+                print (self._sensoryBuffer.text)
+                self._oldLength = len(self._sensoryBuffer.text)
+
+    async def listen(self, end):
+        while True:
+            await asyncio.sleep(0.05)
+            info = end.recv()
+            if info:
+                self._sensoryBuffer.setText(info)
     
     async def run(self):
         #v2t = asyncio.create_task(v2tloop(self._sensoryBuffer))
         d = asyncio.create_task(self._display())
         
+        
 
-        executor = concurrent.futures.ProcessPoolExecutor()
-        executor.submit(v2tloop, self._sensoryBuffer)
-        #executor.submit(self._display)
+        #executor = concurrent.futures.ProcessPoolExecutor()
+        #executor.submit(v2tloop, self._sensoryBuffer)
+        v2t_end, main_end = Pipe()
+        l = asyncio.create_task(self.listen(main_end))
+        p = Process(target = v2tloop, args = (v2t_end,))
+        p.start()
+        #self._sensoryBuffer.setText(main_end.recv())
 
-        await asyncio.gather(d)
+        await asyncio.gather(d, l)
+        p.join()
         # with concurrent.futures.ProcessPoolExecutor() as pool:
         #     await loop.run_in_executor(pool, v2tloop(self._sensoryBuffer))
         # with concurrent.futures.ProcessPoolExecutor() as pool:
