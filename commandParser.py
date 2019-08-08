9957bbaf478b47f5016f0b8b2b401f611c22be9b
Josh Zheng <jz786@cornell.edu>
2019-08-08 15:17:26 -0400

    Support parsing indices

--- commandParser.py
+++ commandParser.py
@@ -12,30 +12,30 @@ class CommandParser():
         # "Your first, second and last cards are white"
 
         word_list = text.split(' ')
-        # if 'index' in word_list:
-        #     i = word_list.index("index")
-        # elif 'indices' in word_list:
-        #     i = word_list.index("indices")
-        # else:
-        #     i = 0
+        if 'index' in word_list:
+            i = word_list.index("index")
+        elif 'indices' in word_list:
+            i = word_list.index("indices")
+        else:
+            i = 0
         
-        # index_list = []
-        # while True:
-        #     i += 1
-        #     try:
-        #         if word_list[i] == 'and':
-        #             continue
-        #         n = int(word_list[i])
-        #         index_list.append(n)
-        #     except:
-        #         break
-        # i += 1 # 'is', 'are'
-        # try:
-        #     feature = int(word_list[i])
-        # except ValueError:
-        #     feature = word_list[i]
+        index_list = []
+        while True:
+            i += 1
+            try:
+                if word_list[i] == 'and':
+                    continue
+                n = int(word_list[i])
+                index_list.append(n)
+            except:
+                break
+        i += 1 # 'is', 'are'
+        try:
+            feature = int(word_list[i])
+        except ValueError:
+            feature = word_list[i]
 
-        feature = word_list[-1]
+        # feature = word_list[-1]
 
         dn = {
             'one': 1,
@@ -47,7 +47,12 @@ class CommandParser():
 
         if feature in dn:
             feature = dn[feature]
+
         print('feature: ' + str(feature))
-        
-        return Hint(PLAYER, feature = feature)
-            
+        print('index list: ' + str(index_list))
+
+        return Hint(PLAYER, feature=feature, indices=index_list)
+
+if __name__ == '__main__':
+    cp = CommandParser()
+    cp.parse('your card at index 3 4 and 5 are 2')
