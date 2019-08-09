2929bd9988d9981470b7164e348a7fe97eaf6c34
nst45 <nst45@cornell.edu>
2019-08-09 13:48:59 -0400

    update action

--- vision/recognizeAction.py
+++ vision/recognizeAction.py
@@ -33,12 +33,12 @@ def recognize_action(prev_state, new_state, permanence):
                             # prev_state = new_state
                             # print('played', action_card)
                             # print('new stable state:', prev_state)
-                            return PlayCard(PLAYER, card_ix = new_state['board'].index(action_card))
+                            return PlaySuccess(PLAYER, action_card, [card_ix = new_state['board'].index(action_card)])
                         if action_card in new_state['discard']: #could also just check top card
                             # self._updateDiscard(new_state)
                             # print('discarded', action_card)
                             # print('new stable state:', self.prev_state)
-                            return Discard(PLAYER, card_ix = new_state['discard'].index(action_card))
+                            return Discard(PLAYER, action_card, [card_ix = new_state['discard'].index(action_card)])
                         else:
                             pass
                             # print('a card just disappeared? very bad')
