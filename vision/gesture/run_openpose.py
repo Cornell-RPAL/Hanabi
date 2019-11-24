cab6f3120f1da333ea657277959c85e078ddcb03
nst45 <noah.scott.thompson@gmail.com>
2019-11-24 16:53:32 -0500

    Integrate gesture with main

--- vision/gesture/run_openpose.py
+++ vision/gesture/run_openpose.py
@@ -4,105 +4,122 @@ import numpy as np
 import cv2
 import time
 
+import asyncio
 from keras.models import load_model
 
-model = load_model('gesture_data/pointing.h5')
+class GestureRec(object):
 
-params = dict()
-params["model_folder"] = "../../../openpose/models/"
-params["hand"] = True
+	def __init__(self):
+		self.model = load_model('/home/cornell.edu/nst45/hanabi_project/Hanabi/vision/gesture/gesture_data/pointing.h5')
 
-# Starting OpenPose
-opWrapper = op.WrapperPython()
-opWrapper.configure(params)
-opWrapper.start()
-datum = op.Datum()
+		self.params = dict()
+		self.params["model_folder"] = "/home/cornell.edu/nst45/hanabi_project/openpose/models/"
+		self.params["hand"] = True
 
-cap = cv2.VideoCapture(0)
+		# Starting OpenPose
+		self.opWrapper = op.WrapperPython()
+		self.opWrapper.configure(self.params)
+		self.opWrapper.start()
+		self.datum = op.Datum()
 
-def indexFromKeypoint(x, y):
-	if (y<250 or y>450):
-		return -1
-	elif x >= 190 and x <= 235:
-		return 0
-	elif x >= 241 and x <= 283:
-		return 1
-	elif x >= 288 and x <= 326:
-		return 2
-	elif x >= 328 and x <= 369:
-		return 3
-	elif x >= 378 and x <= 430:
-		return 4
-	else: return -1
+		self.cap = cv2.VideoCapture(0)
 
-timing = False
-curpose = -1
-start_time = time.time()
+		self.timing = False
+		self.curpose = -1
+		self.start_time = time.time()
 
-indices = []
+		self.indices = []
 
-def getPointingIndices():
-	temp = indices
-	indices = []
-	timing = False
-	curpose = -1
-	return temp
+	def indexFromKeypoint(self, x, y):
+		if (y<250 or y>450):
+			return -1
+		elif x >= 190 and x <= 235:
+			return 0
+		elif x >= 241 and x <= 283:
+			return 1
+		elif x >= 288 and x <= 326:
+			return 2
+		elif x >= 328 and x <= 369:
+			return 3
+		elif x >= 378 and x <= 430:
+			return 4
+		else: return -1
 
-while True:
-	ret, frame = cap.read()
+	# def getPointingIndices():
+	# 	temp = indices
+	# 	indices = []
+	# 	timing = False
+	# 	curpose = -1
+	# 	return temp
 
-	datum.cvInputData = frame
-	opWrapper.emplaceAndPop([datum])
+	async def main(self):
+		while True:
+			#asyncio.sleep(0.05)
+			ret, frame = self.cap.read()
 
-	image = datum.cvOutputData
 
-	cv2.imshow("OpenPose", image)
+			self.datum.cvInputData = frame
+			self.opWrapper.emplaceAndPop([self.datum])
 
-	if datum.poseKeypoints.any():
-		try:
-			p = np.append(datum.poseKeypoints, datum.handKeypoints[0])
-			p = np.append(p, datum.handKeypoints[1])
-			p = p.reshape(1, 67, 3)
-			p = keras.utils.normalize(p, axis = 1)
+			#image = datum.cvOutputData
+			#cv2.imshow("OpenPose", image)
 
-			opt = model.predict_classes(p, batch_size = 67)
-			for j in opt: 
-				if j == 0:
-					print("NO POSE")
-					curpose = -1
-					timing = False
-				elif j == 1:
-					print("point_left")
-					x, y, _ = datum.handKeypoints[0][0][8]
-					index = indexFromKeypoint(x,y)
-					timing = index != -1
-					if (timing and curpose == index):
-						if time.time() - start_time > 0.3 and index not in indices:
-							timing = True
-							indices.append(index)
-					elif (timing):
-						curpose = index
-						start_time = time.time()
-					else: start_time = time.time()
-				elif j == 2:
-					print("point_right")
-					x, y, _ = datum.handKeypoints[1][0][8]
-					index = indexFromKeypoint(x,y)
-					timing = index != -1
-					if (timing and curpose == index):
-						if time.time() - start_time > 0.3 and index not in indices:
-							timing = True
-							indices.append(index)
-					elif (timing):
-						curpose = index
-						start_time = time.time()
-					else: start_time = time.time()
-			print (indices)
-		except:
-			continue
-	key = cv2.waitKey(1)
-	if key == ord('q'):
-		break
+			if self.datum.poseKeypoints.any():
+			
+				try:
+					p = np.append(self.datum.poseKeypoints, self.datum.handKeypoints[0])
+					p = np.append(p, self.datum.handKeypoints[1])
+					p = p.reshape(1, 67, 3)
+					p = keras.utils.normalize(p, axis = 1)
+					opt = self.model.predict_classes(p, batch_size = 67)
 
-cap.release()
-cv2.destroyAllWindows()
+					for j in opt: 
+						if j == 0:
+							print("NO POSE")
+							self.curpose = -1
+							self.timing = False
+						elif j == 1:
+							print("point_left")
+							x, y, _ = self.datum.handKeypoints[0][0][8]
+							index = self.indexFromKeypoint(x,y)
+							self.timing = index != -1
+							if (self.timing and self.curpose == index):
+								if time.time() - self.start_time > 0.3 and index not in self.indices:
+									self.timing = True
+									self.indices.append(index)
+							elif (self.timing):
+								self.curpose = index
+								self.start_time = time.time()
+							else: self.start_time = time.time()
+						elif j == 2:
+							print("point_right")
+							x, y, _ = self.datum.handKeypoints[1][0][8]
+							index = self.indexFromKeypoint(x,y)
+							self.timing = index != -1
+							if (self.timing and self.curpose == index):
+								if time.time() - self.start_time > 0.3 and index not in self.indices:
+									self.timing = True
+									self.indices.append(index)
+							elif (self.timing):
+								self.curpose = index
+								self.start_time = time.time()
+							else: self.start_time = time.time()
+					print (self.indices)
+				except ValueError:
+					continue
+				
+			key = cv2.waitKey(1)
+			if key == ord('q'):
+				break
+
+
+	async def run(self):
+		x = asyncio.create_task(self.main())
+		await asyncio.gather(x)
+
+if __name__ == '__main__':
+	g = GestureRec()
+	asyncio.run(g.run())
+
+	g.cap.release()
+	cv2.destroyAllWindows()
